182c7bdbb8366788203b4ba241873e5c9fe6b7bd
GuanlinWang <43226744+GuanlinWang@users.noreply.github.com>
2019-04-23 20:56:52 -0700

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class digit
 {
-    public:
+public:
     int data;
     digit *next = nullptr;
 };
@@ -32,7 +32,7 @@ int main() {
     char op;
     ofstream outFile ("output.txt");
     ifstream inFile ("largeNumbers.txt");
-
+    
     while (!inFile.eof()){
         left  = writeNum(inFile);
         right = writeNum(inFile);
@@ -42,31 +42,32 @@ int main() {
             break;
         }
         op = getOperator(inFile);
-
+        
         if (op == '+'){
             result = addNumbers(left, right);
-        } else {
-            test = digcmp(left, right);
-            if (test > 0) {
-                result = subNumbers(left, right); // result will be positive
-                result = clearLeadingZeros(result);
-            } else if (test < 0) {
-                result = subNumbers(right, left); // result will be negative
-                result = clearLeadingZeros(result);
-                setNeg(result);
-            } else {
-                result = new digit; // result of subtraction was zero
-            }
         }
+//          else {
+//            test = digcmp(left, right);
+//            if (test > 0) {
+//                result = subNumbers(left, right); // result will be positive
+//                result = clearLeadingZeros(result);
+//            } else if (test < 0) {
+//                result = subNumbers(right, left); // result will be negative
+//                result = clearLeadingZeros(result);
+//                setNeg(result);
+//            } else {
+//                result = new digit; // result of subtraction was zero
+//            }
+//        }
         writeNum(result, outFile);
-        deleteNum(left);
-        deleteNum(right);
-        deleteNum(result);
+       // deleteNum(left);
+       // deleteNum(right);
+        //deleteNum(result);
         left = nullptr;
         right = nullptr;
         result = nullptr;
     }
-
+    
     outFile.close();
     inFile.close();
     return 0;
@@ -74,11 +75,11 @@ int main() {
 
 //-----------------PROVIDED BY INSTRUCTOR-----------------
 /* returns a number <0 || 0 || >0
-*  <0 right number is larger
-*   0  the numbers are equal
-*  >0 left number is larger
-*  only works if the number is stored with lowest significant at front of the list
-*/
+ *  <0 right number is larger
+ *   0  the numbers are equal
+ *  >0 left number is larger
+ *  only works if the number is stored with lowest significant at front of the list
+ */
 int digcmp(digit * left, digit * right){
     int result= 0;
     do // the assignment guarantees there will not be zero length numbers
@@ -105,13 +106,13 @@ int digcmp(digit * left, digit * right){
 //-----------------PROVIDED BY INSTRUCTOR-----------------
 digit * writeNum(ifstream & file){
     if (file.eof())
-    return nullptr;
+        return nullptr;
     digit * head = new digit;
     digit * dig = head;
-
+    
     char aChar;
     int num;
-
+    
     file.get(aChar);
     while (aChar != '\n' && !file.eof()){
         num = int(aChar - '0');
@@ -130,17 +131,17 @@ digit * writeNum(ifstream & file){
 char getOperator(ifstream & file){
     char aChar;
     string toss;
-
+    
     file.get(aChar);
     getline(file, toss);
-
+    
     return aChar;
 }
 
 //-----------------PROVIDED BY INSTRUCTOR-----------------
 void printRecursive(digit * num){
     if (num == nullptr)
-    return;
+        return;
     printRecursive(num->next);
     cout << num->data;
 }
@@ -149,13 +150,13 @@ void printRecursive(digit * num){
 void printNum(digit * num){
     printRecursive(num);
     cout << endl;
-
+    
 }
 
 //-----------------PROVIDED BY INSTRUCTOR-----------------
 void writeRecursive(digit * num, ofstream & file) {
     if (num == nullptr)
-    return;
+        return;
     file << num->data;
     writeRecursive(num->next, file);
 }
@@ -179,50 +180,86 @@ void deleteNum(digit * num){
 
 // TODO: Implement function to add 2 numbers stored in 2 linked lists
 
-digit * addNumbers(digit * left, digit * right){
+digit* addNumbers(digit * left, digit * right)
+{
+    bool end=true;
     digit *result=nullptr;
-    digit *temp=nullptr,*prev=nullptr;
-    int carry=0,sum;
-    while(left!=nullptr&&right!=nullptr)
+    digit *temp=nullptr;
+    digit *head=nullptr;
+    int carry=0,sum=0;
+    while(end)
     {
-        sum=carry+left->data+right->data;
-        if(sum>=10)
+        if(left!=nullptr&&right!=nullptr)
         {
-            carry=1;
-            sum=sum%10; 
+        
+            sum=carry+left->data+right->data;
+            if(sum>=10)
+            {
+                carry=1;
+                sum=sum%10;
+            }
+            else{carry=0;}
         }
-        else{carry=0;}
+        
+        if(left==nullptr&&right!=nullptr)
+        {
+            
+            sum=carry+right->data;
+            if(sum>=10)
+            {
+                carry=1;
+                sum=sum%10;
+            }
+            else{carry=0;}
+        }
+        if(right==nullptr&&left!=nullptr)
+        {
+       
+            sum=carry+left->data;
+            if(sum>=10)
+            {
+                carry=1;
+                sum=sum%10;
+            }
+            else{carry=0;}
+        }
+        //==================================
         temp=new digit;
         temp->data=sum;
         if(result==nullptr)
         {
-            result=temp;
+            result = temp;
         }
-        prev=new digit;
-        prev->next=temp;
-          prev=temp;
-         result->next= new digit;
-        result->next=prev;
-        result=result->next;
+        else
+        {
+            head->next=temp;
+        }
+         head=temp;
+      //==================================
+        if(left!=nullptr)
+        {
         left=left->next;
+        }
+        if(right!=nullptr)
+        {
         right=right->next;
+        }
+        cout<<sum<<" ";
+        if(left==nullptr&&right==nullptr)
+        {
+            break;
+        }
     }
-      while(right!=nullptr)
-      {
-          result->next=new digit;
-          result=result->next;
-          result=right;
-          right=right->next;
-      }
     return result;
 }
+
 
 
 //-----------------PROVIDED BY INSTRUCTOR-----------------
 void subtractCarry(digit * head, digit * prev){
     if (head == nullptr){
         return;
-        } // safety check only, this should never happen!
+    } // safety check only, this should never happen!
     if (head->data == 0){
         head->data = 9;
         subtractCarry(head->next, head);
@@ -243,9 +280,10 @@ digit * clearLeadingZeros(digit * num){
         next = num->next;
         delete num;
         num = next;
-        }
-        return num;
     }
+    return num;
+}
 void setNeg(digit * num){
     num->data = -num->data;
 }
+
